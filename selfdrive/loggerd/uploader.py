d96b929e5f0b64a85552a87032c0d5c4947965eb
Ryan Huber <rhuber@gmail.com>
2018-06-18 03:05:22 +0000

    not interested in helping. xoxo.

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -221,16 +221,16 @@ class Uploader(object):
       os.unlink(fn) # delete the file
       success = True
     else:
-      cloudlog.info("uploading %r", fn)
+      cloudlog.info("not uploading %r", fn)
       # stat = self.killable_upload(key, fn)
-      stat = self.normal_upload(key, fn)
-      if stat is not None and stat.status_code in (200, 201):
-        cloudlog.event("upload_success", key=key, fn=fn, sz=sz)
-        os.unlink(fn) # delete the file
-        success = True
-      else:
-        cloudlog.event("upload_failed", stat=stat, exc=self.last_exc, key=key, fn=fn, sz=sz)
-        success = False
+      #stat = self.normal_upload(key, fn)
+      #if stat is not None and stat.status_code in (200, 201):
+      #  cloudlog.event("upload_success", key=key, fn=fn, sz=sz)
+      os.unlink(fn) # delete the file
+      success = True
+      #else:
+       # cloudlog.event("upload_failed", stat=stat, exc=self.last_exc, key=key, fn=fn, sz=sz)
+        #success = False
 
     self.clean_dirs()
 
